d80a7b1bb6d89cbc1e2764c75e0c8d43a306f3a5
dimka99-10@mail.ru <OXDIMOk_4380>
2023-03-28 20:36:29 +0300

    add: tests

---
+++ dto/src/main/java/com/github/dimka9910/web/example/dto/WebDataObjectDto.java
@@ -0,0 +1,26 @@
+package com.github.dimka9910.web.example.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(name = "WebDataObjectDto", description = "Пример Объета")
+public class WebDataObjectDto {
+
+    @Schema(name = "id", description = "id desc", nullable = true)
+    private Long id;
+    @Schema(name = "data", description = "data desc", nullable = true)
+    private String data;
+
+    private String port;
+    private String host;
+    private String hostName;
+    private String hostNameProperty;
+
+}
